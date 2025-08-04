8e9b97fcb4e5c8bc99bb9d0862e27b19fb16f195
GiovanniMiglio <gmiglio89@gmail.com>
2025-08-05 00:44:08 +0200

    svolto esercizio 1

---
+++ script.js
@@ -0,0 +1,11 @@
+//esercizio1
+
+let Io = {
+    nome : "Giovammi",
+    cognome : "Miglio",
+    eta : 36,
+    saluta(){
+    console.log(`Ciao a tutti sono ${this.nome} ${this.cognome} e ho ${this.eta} anni`)
+    }
+}
+Io.saluta();
